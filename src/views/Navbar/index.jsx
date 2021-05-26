d42bc82013d3344add8712a3a326cdbc2c0f4e9a
Matthew Stinis <mpstinis@gmail.com>
2021-05-26 18:21:06 -0400

    [Views/Nav] nav created and styled

--- src/views/Navbar/index.jsx
+++ src/views/Navbar/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import "./styles.css"
+
 const Navbar = () => {
     return (
         <nav className="nav-container">
-            nav
+            <div className="nav-right">
+                <div className="nav-logo-top">
+                    <div className="text-box">
+                        <span>d</span>
+                        <span>r</span>
+                        <span>o</span>
+                        <span>p</span>
+                    </div>
+                </div>
+                <div className="nav-logo-bottom">
+                    <div className="text-box">
+                        <span>z</span>
+                        <span>o</span>
+                        <span>n</span>
+                        <span>e</span>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
