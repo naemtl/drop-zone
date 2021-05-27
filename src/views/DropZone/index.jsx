86ad87ddea09fbbf80f1204bed1b6df3a4378143
Matthew Stinis <mpstinis@gmail.com>
2021-05-26 23:01:55 -0400

    [Views/Dropzone] styling for layout

--- src/views/DropZone/index.jsx
+++ src/views/DropZone/index.jsx
@@ -7,9 +7,11 @@ import "./styles.css"
 const DropZone = () => {
     return (
         <div className="dropzone-container">
-            <h1 className="dropzone-header">Leezy Boost 103</h1>
-            <CountdownTimer />
-            <div className="dropzone-subheader">Until drop</div>
+            <div className="dropzone-container-inner">
+                <h1 className="dropzone-header">Leezy Boost 103</h1>
+                <CountdownTimer />
+                <div className="dropzone-subheader">Until drop</div>
+            </div>
         </div>
     )
 }
